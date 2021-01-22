ae0351d6608fe07b6870b697d9160e83b3be494b
Jime Pardo <jimenapardo@outlook.com>
2021-01-22 17:33:35 -0300

    sass

--- journal-app/src/components/journal/NothingSelected.js
+++ journal-app/src/components/journal/NothingSelected.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 export const NothingSelected = () => {
     return (
+        <>
         <div className="nothing__main-content">
-            <p>
-                Please select a note  
-                 
-                <br/>
-                <div className="nothing__icon">
-                    <i className="far fa-calendar"></i>
-                </div>
-                
-            </p>
+        <div className="nothing__icon">
+                <i className="far fa-calendar"></i>               
+            </div>
+            
+            <p> Please select a note </p>
+
+           
             
         </div>
+        
+    </>
     )
 }
